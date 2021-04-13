c429fe3a43e327340e597fb3e5e6c397b6d988aa
Phoebe Zeller <phoebe.zeller@hotmail.com>
2021-04-13 16:03:08 +1200

    setup the first files
    this is just getting something on github

---
+++ Circle/sketch.js
@@ -0,0 +1,13 @@
+// code created for the introduction to creative coding workshop (produced for techweek 2021)
+// Author Phoebe Zeller - phoebe.zeller@vuw.ac.nz
+
+function setup(){
+	// create the drawing canvas, save the canvas element
+	createCanvas(960, 500);
+	background(200);
+}
+
+function draw(){
+
+	ellipse(200,200,200);
+}
