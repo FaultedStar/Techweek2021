1908880f1bdc7cce426082bf90dab5ced6b608c3
Phoebe Zeller <phoebe.zeller@hotmail.com>
2021-04-20 16:23:35 +1200

    formatted a little

--- Circle Jitter/sketch.js
+++ Circle Jitter/sketch.js
@@ -1,21 +1,23 @@
 // code created for the introduction to creative coding workshop (produced for techweek 2021)
 // Author Phoebe Zeller - phoebe.zeller@vuw.ac.nz
 
+let circleX = 400;
+let circleY = 200;
+let randomAmount = 10;
+
 function setup(){
 	// create the drawing canvas, save the canvas element
 	createCanvas(960, 500);
 
 }
-let circleX = 400;
-let circleY = 200;
-let randomAmount = 10;
+
 function draw(){
-	background(200,100);
-
-	fill(0);
-	circleX = circleX+random(-randomAmount,randomAmount);
-	 circleY = circleY+random(-randomAmount,randomAmount);
 	let circleSide = 100;
+	background(200,100);
+	fill(0);
 
+	circleX = circleX+random(-randomAmount,randomAmount);
+	circleY = circleY+random(-randomAmount,randomAmount);
+	
 	ellipse(circleX,circleY,circleSide);
 }
