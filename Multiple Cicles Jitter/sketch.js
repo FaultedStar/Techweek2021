9f9f90a6da48f79066bf0a95561985909d072699
Phoebe Zeller <phoebe.zeller@hotmail.com>
2021-04-30 10:48:29 +1200

    lots of jitterbugs

--- Multiple Cicles Jitter/sketch.js
+++ Multiple Cicles Jitter/sketch.js
@@ -3,6 +3,8 @@
 let circlesX = []
 let circlesY = []
 let circlesSize = []
+let randomAmount = 5;
+
 function setup(){
 	// create the drawing canvas, save the canvas element
 	createCanvas(960, 500);
@@ -13,12 +15,15 @@ function setup(){
 		circlesSize.push(random(50,250));
 
 	}
-
 }
 
 function draw(){
+		background(200);
 	fill(0);
 	for(let i = 0; i < 10; i++){
 	ellipse(circlesX[i],circlesY[i],circlesSize[i]);
+	circlesX[i] = circlesX[i] + random(-randomAmount,randomAmount);
+	circlesY[i] = circlesY[i] + random(-randomAmount,randomAmount);
+
 	}
 }
